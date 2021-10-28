469be7e431b75d8872d2dc83fcc6d8934db6fabe
urain39 <urain39@qq.com>
2021-10-29 05:35:13 +0800

    add --fuzzy for renamings

--- my-zram-config.sh
+++ my-zram-config.sh
@@ -8,7 +8,7 @@ LOG_COMP_ALGO="lz4"
 RAM_HOME="off"
 HOME_DIR="/home"
 STATIC_DIR="/static"
-RSYNC_ARGS="-au --inplace --no-whole-file --delete-after"
+RSYNC_ARGS="-auy --inplace --no-whole-file --delete-after"
 
 _NAME=my-zram-config
 _CFG_PATH=/etc/"$_NAME"/config.conf
