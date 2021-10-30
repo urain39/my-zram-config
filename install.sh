15a1d273db881592e48ade981b0418c510abcffe
μrainЗ⑨ <16981964+urain39@users.noreply.github.com>
2021-10-30 08:56:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 install -m755 my-zram-config.sh /usr/local/bin/my-zram-config
 install -Dm644 etc/my-zram-config/config.conf /etc/my-zram-config/config.conf
-install -m755 openrc/my-zram-config.in /etc/init.d/my-zram-config
+install -m755 openrc/my-zram-config.initd /etc/init.d/my-zram-config
 
 rc-update del my-zram-config boot 2> /dev/null
 rc-update del my-zram-config default 2> /dev/null
